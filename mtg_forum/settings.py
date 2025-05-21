815c05c0e567fc720b07f91ed7faae59c43293f9
Arcandrus <eric.harper87@gmail.com>
2025-05-21 15:17:51 +0100

    Added whitenoise

--- mtg_forum/settings.py
+++ mtg_forum/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
