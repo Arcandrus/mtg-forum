d03d6d781a22ecf89d7caf3a5847f4efdb40290b
Arcandrus <eric.harper87@gmail.com>
2025-05-30 15:03:20 +0100

    Added pagination numbers to index.html

--- staticfiles/js/messages.js
+++ staticfiles/js/messages.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const messages = document.querySelectorAll('.messages li');
     if (messages.length) {
-      setTimeout(() => {
-        messages.forEach(msg => {
-          msg.style.opacity = '0';
-        });
-      }, 2000);
-      
-      setTimeout(() => {
-        const messagesContainer = document.querySelector('.messages');
-        if (messagesContainer) {
-          messagesContainer.remove();
-        }
-      }, 6000);
+        setTimeout(() => {
+            messages.forEach(msg => {
+                msg.style.opacity = '0';
+            });
+        }, 2000);
+
+        setTimeout(() => {
+            const messagesContainer = document.querySelector('.messages');
+            if (messagesContainer) {
+                messagesContainer.remove();
+            }
+        }, 6000);
     }
-  });
+});
